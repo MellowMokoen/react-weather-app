b3477c4a14de533730f8d924fd55d2afb0299e2b
MellowMokoen <mamello3632@gmail.com>
2024-02-19 09:49:01 +0200

    Formed the weather UI

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,43 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="weather">hellow from the weather</div>;
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-secondary" />
+          </div>
+        </div>
+      </form>
+      <h1>Cape Town</h1>
+      <ul>
+        <li>Wednesday 08:00</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly cloudy"
+          />
+          6 C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 5</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
