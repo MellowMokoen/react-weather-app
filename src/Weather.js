83f2b217efcac67e154590c05b9f239285e8912f
MellowMokoen <mamello3632@gmail.com>
2024-02-19 12:26:24 +0200

    Created functions to handle the api data

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-secondary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Cape Town</h1>
-      <ul>
-        <li>Wednesday 08:00</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row mt-4">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Mostly cloudy"
-              className="float-left"
-            />
+  const [ready, setready] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setready(true);
+  }
 
-            <span className="temperature">6</span>
-            <span className="unit">°C</span>
+  if (ready) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-secondary w-100"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 5 km/h</li>
-          </ul>
+        </form>
+        <h1>Cape Town</h1>
+        <ul>
+          <li>Wednesday 08:00</li>
+          <li>Mostly cloudy</li>
+        </ul>
+        <div className="row mt-4">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly cloudy"
+                className="float-left"
+              />
+
+              <span className="temperature">{Math.round(temperature)}</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 5 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "5c3947ac0468bdb0b2d7714c9e35d56e";
+    let city = "Cape Town";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
